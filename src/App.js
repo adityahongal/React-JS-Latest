74c6de10a0a69de14769e49d3d75ccac515a43b6
aditya <adityahongalwork@gmail.com>
2023-12-25 15:31:16 +0530

    ToDo project

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import { nanoid } from "nanoid";
+
+
+function App({
+  tasksData,
+}) {
+  const [tasks, setTasks] = useState(tasksData);
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+  
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+    
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  
+
+  const taskList = tasks.map((task) => <Todo 
+    name={task.name} 
+    completed={task.completed} 
+    id={task.id} 
+    toggleTaskCompleted={toggleTaskCompleted} 
+    deleteTask={deleteTask} 
+    editTask={editTask}
+    />);
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+      
+        
+     {taskList}
+
+      </ul>
+    </div>
+  );
+}
+
+export default App
